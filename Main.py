6a97d62079c2b4b2a421a669c6a3689f559204c6
GowthamRayappa <118067700+GowthamRayappa@users.noreply.github.com>
2023-02-10 22:25:04 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from gpiozero import LED
+led = LED(2)
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    if led.value == 1:
+        status = 'ON'
+    else:
+        status = 'OFF'
+    return render_template('home.html', status=status)
+
+
+@app.route("/on")
+
+def on():
+    led.on()
+    return "LED on"
+
+@app.route("/off")
+
+def off():
+    led.off()
+    return "LED off"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000) 
+
